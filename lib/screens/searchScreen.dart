e18a669e7a62f9298fa037434023017d5cbb2112
Omar Mach <omarmachhouty@gmail.com>
2021-05-03 21:24:09 +0100

    pagination done

--- lib/screens/searchScreen.dart
+++ lib/screens/searchScreen.dart
@@ -156,72 +156,168 @@ class _SearchScreenState extends State<SearchScreen> {
                   },
                 ),
                 // Results widget
-                Consumer<SearchProvider>(
-                  builder: (context, searchProvider, child) {
-                    final List<WooProduct> searchedProducts =
-                        searchProvider.searchedProducts;
-
-                    return searchedProducts.length > 0
-                        ? Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Divider(),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  "Search results",
-                                  textAlign: TextAlign.center,
-                                  style: textTheme.headline6,
-                                ),
-                              ),
-                              Divider(),
-                              ElevatedButton.icon(
-                                onPressed: searchProvider
-                                        .searchedProducts.isEmpty
-                                    ? null
-                                    : () {
-                                        searchProvider.clearSearchedProducts();
-                                      },
-                                label: FittedBox(
-                                  fit: BoxFit.fitWidth,
-                                  child: Text("Clear results"),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.grey.shade800,
-                                ),
-                                icon: Icon(Icons.delete),
-                              ),
-                              GridView.builder(
-                                shrinkWrap: true,
-                                gridDelegate:
-                                    SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  childAspectRatio: 0.55,
-                                  mainAxisSpacing: 10,
-                                ),
-                                physics: NeverScrollableScrollPhysics(),
-                                itemCount: searchedProducts.length,
-                                itemBuilder: (context, index) => Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ProductWidget(
-                                    product: searchedProducts[index],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          )
-                        : isLoading
-                            ? Center(
-                                child: CircularProgressIndicator(),
-                              )
-                            : Container();
-                  },
-                )
+                SearchByCategoryResultsWidget(
+                    textTheme: textTheme, isLoading: isLoading)
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class SearchByCategoryResultsWidget extends StatefulWidget {
+  const SearchByCategoryResultsWidget({
+    Key key,
+    @required this.textTheme,
+    @required this.isLoading,
+  }) : super(key: key);
+
+  final TextTheme textTheme;
+  final bool isLoading;
+
+  @override
+  _SearchByCategoryResultsWidgetState createState() =>
+      _SearchByCategoryResultsWidgetState();
+}
+
+class _SearchByCategoryResultsWidgetState
+    extends State<SearchByCategoryResultsWidget> {
+  int page;
+  @override
+  void initState() {
+    page = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<SearchProvider>(
+      builder: (context, searchProvider, child) {
+        final List<WooProduct> searchedProducts =
+            searchProvider.searchedProducts;
+        int productsCount = searchedProducts.length;
+        int pagesCount = productsCount ~/ 10;
+        int remainingProductsCount = productsCount % 10;
+        if (remainingProductsCount > 0) pagesCount++;
+
+        print("productsCount : $productsCount");
+        print("pagesCount : $pagesCount");
+        print("remainingProductsCount : $remainingProductsCount");
+
+        return searchedProducts.length > 0
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Divider(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Search results",
+                      textAlign: TextAlign.center,
+                      style: widget.textTheme.headline6,
+                    ),
+                  ),
+                  Divider(),
+                  ElevatedButton.icon(
+                    onPressed: searchProvider.searchedProducts.isEmpty
+                        ? null
+                        : () {
+                            searchProvider.clearSearchedProducts();
+                          },
+                    label: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text("Clear results"),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey.shade800,
+                    ),
+                    icon: Icon(Icons.delete),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text("Previous"),
+                          onPressed: page > 0
+                              ? () {
+                                  setState(() {
+                                    page--;
+                                  });
+                                }
+                              : null,
+                        ),
+                      ),
+                      horizontalSeparator,
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text("Next"),
+                          onPressed: page < pagesCount - 1
+                              ? () {
+                                  setState(() {
+                                    page++;
+                                  });
+                                }
+                              : null,
+                        ),
+                      ),
+                    ],
+                  ),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.55,
+                      mainAxisSpacing: 10,
+                    ),
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount:
+                        page == pagesCount - 1 ? remainingProductsCount : 10,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ProductWidget(
+                        product: searchedProducts[page * 10 + index],
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text("Previous"),
+                          onPressed: page > 0
+                              ? () {
+                                  setState(() {
+                                    page--;
+                                  });
+                                }
+                              : null,
+                        ),
+                      ),
+                      horizontalSeparator,
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text("Next"),
+                          onPressed: page < pagesCount - 1
+                              ? () {
+                                  setState(() {
+                                    page++;
+                                  });
+                                }
+                              : null,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : widget.isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Container();
+      },
     );
   }
 }
