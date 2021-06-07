8f05a3d8965219f3de6bd447ab387fd5547d2ea9
Omar Mach <omarmachhouty@gmail.com>
2021-06-07 10:48:53 +0100

    clear fav, background to favs, minor fixes

--- lib/screens/favoritesScreen.dart
+++ lib/screens/favoritesScreen.dart
@@ -19,44 +19,93 @@ class FavoritesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBarWidget(),
       drawer: DrawerMenuWidget(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.contain,
+            repeat: ImageRepeat.repeat,
+            image: AssetImage('assets/images/background.jpg'),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
               child: Consumer<FavoritesProvider>(
                 builder: (context, favoritesProvider, _) {
-                  return favoritesProvider.favorites.isNotEmpty
-                      ? FavoritesGridList(
-                          favoritesList: favoritesProvider.favorites,
-                        )
-                      : Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.star_border_rounded,
-                                size: size.width / 3,
+                  if (favoritesProvider.favorites.isNotEmpty) {
+                    return SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "My Favorites",
+                                style: textTheme.headline4,
                               ),
-                              Text(
-                                "Your wish list is empty",
-                                style: textTheme.subtitle1,
-                              ),
-                              verticalSeparator,
-                              Text(
-                                "You can add items to your whishlist, through the ❤️ icon from any product details.",
-                                style: textTheme.caption,
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
+                            ),
+                            Text(
+                              "This list will help you track your favorite products.",
+                              style: textTheme.caption,
+                            ),
+                            verticalSeparator,
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: OutlinedButton(
+                                    style: OutlinedButton.styleFrom(
+                                      primary: Colors.redAccent,
+                                      side: BorderSide(
+                                        color: Colors.redAccent,
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      favoritesProvider.clearFavorites();
+                                    },
+                                    child: Text("Clear Favorites"),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            FavoritesGridList(
+                              favoritesList: favoritesProvider.favorites,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.star_border_rounded,
+                            size: size.width / 3,
                           ),
-                        );
+                          Text(
+                            "Your wish list is empty",
+                            style: textTheme.subtitle1,
+                          ),
+                          verticalSeparator,
+                          Text(
+                            "You can add items to your whishlist, through the ❤️ icon from any product details.",
+                            style: textTheme.caption,
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    );
+                  }
                 },
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
