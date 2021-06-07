47b0625874dd374dd061bde3578c80e58b52cb91
Omar Mach <omarmachhouty@gmail.com>
2021-06-07 10:18:34 +0100

    fixed homescreen lines

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -31,58 +31,60 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         child: SafeArea(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.contain,
-                    repeat: ImageRepeat.repeat,
-                    image: AssetImage('assets/images/background.jpg'),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                repeat: ImageRepeat.repeat,
+                image: AssetImage('assets/images/background.jpg'),
+              ),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CarouselWidget(),
+                      verticalSeparator,
+                      HomeTitleWidget(title: "New Arrivals"),
+                      ProductsGridList(
+                        limit: 6,
+                      ),
+                      HomeTitleWidget(title: "Screen Refurbish Tools"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ProductsByCategoryGridList(
+                          categoryId: 71,
+                          limit: 6,
+                        ),
+                      ),
+                      HomeTitleWidget(title: "Soldering accessories"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ProductsByCategoryGridList(
+                          limit: 6,
+                          categoryId: 79,
+                        ),
+                      ),
+                      verticalSeparator,
+                      HomeTitleWidget(title: "Coming Soon"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ComingSoonCarouselWidget(),
+                      ),
+                      // verticalSeparator,
+                      // ClipRRect(
+                      //   borderRadius: BorderRadius.circular(10),
+                      //   child: Image.asset(
+                      //     'assets/images/proff.jpg',
+                      //     width: size.width,
+                      //     fit: BoxFit.fill,
+                      //   ),
+                      // ),
+                    ],
                   ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    CarouselWidget(),
-                    verticalSeparator,
-                    HomeTitleWidget(title: "New Arrivals"),
-                    ProductsGridList(
-                      limit: 6,
-                    ),
-                    HomeTitleWidget(title: "Screen Refurbish Tools"),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ProductsByCategoryGridList(
-                        categoryId: 71,
-                        limit: 6,
-                      ),
-                    ),
-                    HomeTitleWidget(title: "Soldering accessories"),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ProductsByCategoryGridList(
-                        limit: 6,
-                        categoryId: 79,
-                      ),
-                    ),
-                    verticalSeparator,
-                    HomeTitleWidget(title: "Coming Soon"),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ComingSoonCarouselWidget(),
-                    ),
-                    // verticalSeparator,
-                    // ClipRRect(
-                    //   borderRadius: BorderRadius.circular(10),
-                    //   child: Image.asset(
-                    //     'assets/images/proff.jpg',
-                    //     width: size.width,
-                    //     fit: BoxFit.fill,
-                    //   ),
-                    // ),
-                  ],
                 ),
               ),
             ),
@@ -100,49 +102,37 @@ class HomeTitleWidget extends StatelessWidget {
     Key key,
     @required this.title,
   }) : super(key: key);
-  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      _scrollController.jumpTo(_scrollController.position.maxScrollExtent / 2);
-    });
-
     final size = MediaQuery.of(context).size;
 
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        controller: _scrollController,
-        physics: NeverScrollableScrollPhysics(),
-        child: SizedBox(
-          width: size.width * 1.2,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              redBox,
-              redBox,
-              redBox,
-              redBox,
-              InkWell(
-                onTap: () {
-                  _scrollController
-                      .jumpTo(_scrollController.position.maxScrollExtent / 2);
-                },
-                child: Text(
-                  title,
-                  style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              redBox,
-              redBox,
-              redBox,
-              redBox,
-            ],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: Divider(
+              color: Colors.redAccent,
+              thickness: 2,
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.headline6,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Divider(
+              color: Colors.redAccent,
+              thickness: 2,
+            ),
+          ),
+        ],
       ),
     );
   }
